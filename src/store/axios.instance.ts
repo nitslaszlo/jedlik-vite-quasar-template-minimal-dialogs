4d2bc10586103978f07fd6cdca3c3526ba000652
nitslaszlo <nitslaszlo@gmail.com>
2022-10-29 22:34:29 +0200

    up

--- src/store/axios.instance.ts
+++ src/store/axios.instance.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-// const $axios = axios.create({
-//   baseURL: "https://jedlik-backend.cyclic.app/",
-//   withCredentials: true,
-// });
-
 const $axios = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://jedlik-backend.cyclic.app/",
   withCredentials: true,
 });
+
+// const $axios = axios.create({
+//   baseURL: "http://localhost:5000/",
+//   withCredentials: true,
+// });
 
 export default $axios;
